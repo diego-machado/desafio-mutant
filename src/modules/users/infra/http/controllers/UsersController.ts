3024dabb182d33038a2626267422495ef28d043d
Diego Machado <diego.machadodarosa@gmail.com>
2020-09-30 19:41:03 -0300

    chore: eslint config

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 
 export default class UsersController {
-
-  public async create (request: Request, response: Response): Promise<Response> {
-    return response.json({users: 'teste'});
+  public async create(request: Request, response: Response): Promise<Response> {
+    return response.json({ users: 'teste' });
   }
 }
